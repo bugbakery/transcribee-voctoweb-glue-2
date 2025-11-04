484bc11e68126f41fe2251eae83ba43f7a1a2e8f
Philipp Mandler <info@philipp-mandler.com>
2025-11-04 17:16:10 +0100

    Add cache
    this also fixes scroll restoration

--- frontend/src/pb.ts
+++ frontend/src/pb.ts
@@ -1,25 +1,42 @@
 import Pocketbase, { type RecordFullListOptions, type RecordOptions } from 'pocketbase';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 export const pb = new Pocketbase('/');
 
+const cache = new Map<string, any>();
+const fromCache = (key: any) => cache.get(JSON.stringify(key)) || null;
+const toCache = (key: any, value: any) => cache.set(JSON.stringify(key), value);
+
 export const useGetFullList = (collection: string, opts: RecordFullListOptions) => {
-  const [data, setData] = useState<null | any[]>(null);
+  const [data, setData] = useState<null | any[]>(fromCache(['getFullList', collection, opts]));
 
   useEffect(() => {
-    pb.collection(collection).getFullList(opts).then(setData).catch(console.error);
+    pb.collection(collection)
+      .getFullList(opts)
+      .then((data) => {
+        toCache(['getFullList', collection, opts], data);
+        setData(data);
+      })
+      .catch(console.error);
   }, [collection, JSON.stringify(opts)]);
 
   return data;
 };
 
 export const useGetOne = (collection: string, id: string | undefined, opts?: RecordOptions) => {
-  const [data, setData] = useState<null | any>(null);
+  const [data, setData] = useState<null | any>(fromCache(['getOne', collection, id, opts]));
+
+  const load = useCallback(() => {
+    if (!id) return;
+    pb.collection(collection).getOne(id, opts).then((data) => {
+      toCache(['getOne', collection, id, opts], data);
+      setData(data);
+    }).catch(console.error);
+  }, [id, collection, JSON.stringify(opts)]);
 
   useEffect(() => {
-    if (!id) return;
-    pb.collection(collection).getOne(id, opts).then(setData).catch(console.error);
-  }, [collection, id, JSON.stringify(opts)]);
+    load();
+  }, [load]);
 
-  return data;
+  return { data, refresh: load };
 };
