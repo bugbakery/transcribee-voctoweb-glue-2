db8e06e4c076f0db0d568820b10d7235b9dc8131
Jaro Habiger <jarohabiger@googlemail.com>
2025-11-19 14:06:21 +0100

    add missing persons migration

---
+++ migrations/1763557095_updated_talks.go
@@ -0,0 +1,41 @@
+package migrations
+
+import (
+	"github.com/pocketbase/pocketbase/core"
+	m "github.com/pocketbase/pocketbase/migrations"
+)
+
+func init() {
+	m.Register(func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("pbc_2355380017")
+		if err != nil {
+			return err
+		}
+
+		// add field
+		if err := collection.Fields.AddMarshaledJSONAt(16, []byte(`{
+			"hidden": false,
+			"id": "json2723989459",
+			"maxSize": 0,
+			"name": "persons",
+			"presentable": false,
+			"required": false,
+			"system": false,
+			"type": "json"
+		}`)); err != nil {
+			return err
+		}
+
+		return app.Save(collection)
+	}, func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("pbc_2355380017")
+		if err != nil {
+			return err
+		}
+
+		// remove field
+		collection.Fields.RemoveById("json2723989459")
+
+		return app.Save(collection)
+	})
+}
