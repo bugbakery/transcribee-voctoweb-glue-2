cf5c660ee579b07450673913036ce4daf823251a
Philipp Mandler <info@philipp-mandler.com>
2025-11-21 14:05:46 +0100

    TalkPage: move side panel to top

--- frontend/src/pages/TalkPage.tsx
+++ frontend/src/pages/TalkPage.tsx
@@ -8,62 +8,61 @@ export const TalkPage = () => {
 
   return (
     <div className="mx-8">
-      <div>
-        <h1 className="text-2xl font-bold mb-4">{talk.title}</h1>
-        <h2 className="text-lg font-bold mb-4">{talk.subtitle}</h2>
-        <div className="flex flex-row mb-4">
-          <InfoFieldH label={talk.persons.length > 1 ? 'Speakers' : 'Speaker'}>
-            {talk.persons.join(', ')}
-          </InfoFieldH>
-          <InfoFieldH label="Duration">{formatDuration(talk.duration_secs)}</InfoFieldH>
-          <InfoFieldH label="Event">{talk.expand?.conference?.name}</InfoFieldH>
-        </div>
-        <div className="flex gap-10 items-start">
+      <div className="flex gap-10 items-start">
+        <div>
+          <h1 className="text-2xl font-bold mb-4">{talk.title}</h1>
+          <h2 className="text-lg font-bold mb-4">{talk.subtitle}</h2>
+          <div className="flex flex-row mb-4">
+            <InfoFieldH label={talk.persons.length > 1 ? 'Speakers' : 'Speaker'}>
+              {talk.persons.join(', ')}
+            </InfoFieldH>
+            <InfoFieldH label="Duration">{formatDuration(talk.duration_secs)}</InfoFieldH>
+            <InfoFieldH label="Event">{talk.expand?.conference?.name}</InfoFieldH>
+          </div>
           <div>{talk.description}</div>
-
-          <div className="min-w-80">
-            <div className="flex justify-end gap-3 mb-4">
-              {talk.assignee === pb.authStore.record?.id ? (
-                <>
-                  <a
-                    href={talk.transcribee_url + `&speaker_name_options=${talk.persons.join(',')}`}
-                    target="_blank"
-                    className="bg-white/80 border border-white text-black hover:bg-white text-sm font-semibold py-1 px-2 rounded-lg"
-                  >
-                    Open Editor
-                  </a>
-                  <Button
-                    type="button"
-                    onClick={async () => {
-                      await pb.collection('talks').update(talk.id, { assignee: null });
-                      mutate();
-                    }}
-                  >
-                    Finish Work
-                  </Button>
-                </>
-              ) : (
-                <>
-                  <Button
-                    type="button"
-                    onClick={async () => {
-                      await pb
-                        .collection('talks')
-                        .update(talk.id, { assignee: pb.authStore.record?.id });
-                      mutate();
-                    }}
-                  >
-                    Claim
-                  </Button>
-                </>
-              )}
-            </div>
-            <div className="p-8 border border-white/16 bg-white/5 rounded-2xl">
-              <InfoField label="Assignee">{talk.expand?.assignee?.username || '-'}</InfoField>
-              <InfoField label="Corrected until">
-                {formatDuration(talk.corrected_until_secs)}
-              </InfoField>
-            </div>
+        </div>
+        <div className="min-w-80">
+          <div className="flex justify-end gap-3 mb-4">
+            {talk.assignee === pb.authStore.record?.id ? (
+              <>
+                <a
+                  href={talk.transcribee_url + `&speaker_name_options=${talk.persons.join(',')}`}
+                  target="_blank"
+                  className="bg-white/80 border border-white text-black hover:bg-white text-sm font-semibold py-1 px-2 rounded-lg"
+                >
+                  Open Editor
+                </a>
+                <Button
+                  type="button"
+                  onClick={async () => {
+                    await pb.collection('talks').update(talk.id, { assignee: null });
+                    mutate();
+                  }}
+                >
+                  Finish Work
+                </Button>
+              </>
+            ) : (
+              <>
+                <Button
+                  type="button"
+                  onClick={async () => {
+                    await pb
+                      .collection('talks')
+                      .update(talk.id, { assignee: pb.authStore.record?.id });
+                    mutate();
+                  }}
+                >
+                  Claim
+                </Button>
+              </>
+            )}
+          </div>
+          <div className="p-8 border border-white/16 bg-white/5 rounded-2xl">
+            <InfoField label="Assignee">{talk.expand?.assignee?.username || '-'}</InfoField>
+            <InfoField label="Corrected until">
+              {formatDuration(talk.corrected_until_secs)}
+            </InfoField>
           </div>
         </div>
       </div>
