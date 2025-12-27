8f2c86a22176fe663fe2f7cb95fc251ce6fd3971
Philipp Mandler <info@philipp-mandler.com>
2025-12-27 15:31:33 +0100

    nix: Build frontend

--- nix/pkgs/backend.nix
+++ nix/pkgs/backend.nix
@@ -1,10 +1,41 @@
 {
   pkgs,
+  lib,
   ...
 }:
+let
+  frontend = pkgs.buildNpmPackage (
+    lib.fix (self: {
+      pname = "transcribee-voctoweb-frontend";
+      version = "0.1.0";
+      src = ../../frontend;
+
+      nativeBuildInputs = [ pkgs.git ];
+
+      npmDeps = pkgs.importNpmLock {
+        npmRoot = self.src;
+      };
+
+      installPhase = ''
+        runHook preInstall
+        cp -r dist $out
+        runHook postInstall
+      '';
+
+      npmBuildScript = "build";
+
+      npmConfigHook = pkgs.importNpmLock.npmConfigHook;
+    })
+  );
+in
 pkgs.buildGoModule {
   pname = "transcribee-voctoweb";
   version = "0.1.0";
   src = ../..;
   vendorHash = "sha256-HfkWQw6bU5UglgzTmSiclYl6yaacjt6qFyoDgdMDH/o=";
+
+  postInstall = ''
+    mkdir -p $out/frontend
+    cp -R ${frontend} $out/frontend/dist
+'';
 }
